f4c7271fbce74db4adec7c452f8fd9cc86950758
Daegyeom Ha <combbm@gmail.com>
2024-12-13 14:08:47 +0900

    fix: remove description content

--- src/components/RssCard/index.tsx
+++ src/components/RssCard/index.tsx
@@ -10,7 +10,6 @@ const RssCard: Component<RssCardProps> = (props) => {
   return (
     <div class="w-full space-y-2 rounded-md border border-gray-200 p-3">
       <h1 class="text-xl font-bold">{props.title}</h1>
-      <p>{props.description}</p>
       <p>
         자세히보기:{' '}
         <a href={props.link} target="_blank" rel="noreferrer" class="text-blue-800 underline">
